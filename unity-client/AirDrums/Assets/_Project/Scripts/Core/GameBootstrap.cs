fa6a5df93e3a82287e3f4212df4cca24be3d5f2f
Jean Paul Ari Maldonado <jean.ari@ucsp.edu.pe>
2026-03-27 02:31:01 -0500

    improvement/ adding Core scripts

---
+++ unity-client/AirDrums/Assets/_Project/Scripts/Core/GameBootstrap.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace AirDrums.Core
+{
+    public class GameBootstrap : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("AirDrums bootstrap initialized.");
+        }
+    }
+}
